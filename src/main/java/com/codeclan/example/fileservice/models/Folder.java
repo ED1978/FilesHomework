8d85b706fc4627112eb0db30dbe7ed6a795f38c0
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-19 14:49:44 +0000

    Folder annotated with one2many join with File

--- src/main/java/com/codeclan/example/fileservice/models/Folder.java
+++ src/main/java/com/codeclan/example/fileservice/models/Folder.java
@@ -1,12 +1,23 @@
 package com.codeclan.example.fileservice.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
+@Table(name = "folders")
 public class Folder {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    @Column(name = "title")
     private String title;
+    @JsonIgnoreProperties("folder")
+    @OneToMany(mappedBy = "folder")
     private List<File> files;
 
     public Folder(String title){
