febf5db4a9ccd0db244799be371206167aa2edfe
EDownie <53910027+ED1978@users.noreply.github.com>
2019-11-19 14:38:28 +0000

    Folder created with methods

---
+++ src/main/java/com/codeclan/example/fileservice/models/Folder.java
@@ -0,0 +1,48 @@
+package com.codeclan.example.fileservice.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Folder {
+
+    private Long id;
+    private String title;
+    private List<File> files;
+
+    public Folder(String title){
+        this.title = title;
+        this.files = new ArrayList<File>();
+    }
+
+    public Folder(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public void addFile(File file){
+        this.files.add(file);
+    }
+}
